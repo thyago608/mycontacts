66a7a7c79b3608f84beb142018919cc71a06123b
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-10-31 16:51:56 -0400

    Refactoring Method Create

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -1,4 +1,5 @@
 const { v4 } = require("uuid");
+const db = require("../../database");
 
 let contacts = [
   {
@@ -56,21 +57,18 @@ class ContactRepository {
   }
 
   async create(user) {
-    return new Promise((resolve) => {
-      const { name, email, phone, category_id } = user;
+    const { name, email, phone, category_id } = user;
 
-      const newContact = {
-        id: v4(),
-        name,
-        email,
-        phone,
-        category_id,
-      };
+    const [row] = await db.query(
+      `
+      INSERT INTO contacts(name, email, phone, category_id)
+      VALUES($1, $2, $3, $4)
+      RETURNING *
+      `,
+      [name, email, phone, category_id]
+    );
 
-      contacts.push(newContact);
-
-      resolve(newContact);
-    });
+    return row;
   }
 
   async update(id, user) {
